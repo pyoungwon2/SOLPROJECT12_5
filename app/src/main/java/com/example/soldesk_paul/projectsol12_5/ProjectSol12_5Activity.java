48c1945c0be80c91b5452ecdbe6c49124016bb68
pyoungwon2 <pyoungwon2@naver.com>
2016-09-06 09:43:15 +0900

    솔데스크 프로젝트 5팀

---
+++ app/src/main/java/com/example/soldesk_paul/projectsol12_5/ProjectSol12_5Activity.java
@@ -0,0 +1,13 @@
+package com.example.soldesk_paul.projectsol12_5;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProjectSol12_5Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_project_sol12_5);
+    }
+}
